f00a61a33087e2b40cdefe016c00208501fc02ee
refaatbasant2030 <refaatbasant2030@gmail.com>
2025-08-02 23:21:51 +0300

    add DAL and sql Table

---
+++ ADO_Assignment7/DAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO_Assignment7
+{
+    public static class DAL
+    {
+        
+
+        public DAL()
+        {
+
+        }
+
+    }
+}
